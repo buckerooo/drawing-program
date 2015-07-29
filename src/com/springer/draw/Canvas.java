cce80ba59164183e1ae52d6f53905e48129264ae
buck <buck@buckeroo.com>
2015-07-29 21:39:03 +0100

    buck: moved creation of canvas onto the canvas class

--- src/com/springer/draw/Canvas.java
+++ src/com/springer/draw/Canvas.java
@@ -7,6 +7,20 @@ import java.util.List;
 public class Canvas {
     private List<List<Character>> canvas = new ArrayList<>();
 
+    public void create(int width, int height) {
+        canvas.add(charsOfSize(width + 2, '-'));
+
+        for(int rowPosition = 1; rowPosition <= height; rowPosition++) {
+            List<Character> row = new ArrayList<>();
+            row.addAll(charsOfSize(1, '|'));
+            row.addAll(charsOfSize(width, ' '));
+            row.addAll(charsOfSize(1, '|'));
+            canvas.add(rowPosition, row);
+        }
+
+        canvas.add(height + 1, charsOfSize(width + 2, '-'));
+    }
+
     public void fill(Point from, Point to, char x) {
         /* This bit takes care of the horizontal line */
         List<Character> row = canvas.get(from.y);
@@ -17,12 +31,8 @@ public class Canvas {
         allRowsForVerticalLine.forEach(row1 -> row1.subList(from.x, to.x + 1).replaceAll(character -> x));
     }
 
-    public void add(List<Character> characters) {
-        canvas.add(characters);
-    }
-
-    public void add(int rowPosition, List<Character> row) {
-        canvas.add(rowPosition, row);
+    public void fill(Point point, char fillColor) {
+        fill(point, point, fillColor);
     }
 
     public void printCanvas(PrintStream printStream) {
@@ -32,11 +42,17 @@ public class Canvas {
         });
     }
 
-    public void fill(Point point, char fillColor) {
-        fill(point, point, fillColor);
-    }
-
     public boolean isEmptySpace(Point point) {
         return canvas.get(point.y).get(point.x) == ' ';
+    }
+
+    private List<Character> charsOfSize(int width, char c) {
+        List<Character> chars = new ArrayList<>();
+
+        for(int x = 0; x < width; x++) {
+            chars.add(c);
+        }
+
+        return chars;
     }
 }
--- src/com/springer/draw/CreateCanvas.java
+++ src/com/springer/draw/CreateCanvas.java
@@ -1,8 +1,5 @@
 package com.springer.draw;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CreateCanvas implements DrawCommand {
 
     private final Canvas canvas;
@@ -17,26 +14,6 @@ public class CreateCanvas implements DrawCommand {
         int width = Integer.valueOf(inputs[0]);
         int height = Integer.valueOf(inputs[1]);
 
-        canvas.add(charsOfSize(width + 2, '-'));
-
-        for(int rowPosition = 1; rowPosition <= height; rowPosition++) {
-            List<Character> row = new ArrayList<>();
-            row.addAll(charsOfSize(1, '|'));
-            row.addAll(charsOfSize(width, ' '));
-            row.addAll(charsOfSize(1, '|'));
-            canvas.add(rowPosition, row);
-        }
-
-        canvas.add(height + 1, charsOfSize(width + 2, '-'));
-    }
-
-    private List<Character> charsOfSize(int width, char c) {
-        List<Character> chars = new ArrayList<>();
-
-        for(int x = 0; x < width; x++) {
-            chars.add(c);
-        }
-
-        return chars;
+        canvas.create(width, height);
     }
 }

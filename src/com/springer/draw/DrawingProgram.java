d3b482eec734b32bf9972e7abe2aaba92ad4420d
buck <buck@buckeroo.com>
2015-08-30 14:57:45 +0100

    buck: moving things about a little

--- src/com/springer/draw/DrawingProgram.java
+++ src/com/springer/draw/DrawingProgram.java
@@ -1,8 +1,5 @@
 package com.springer.draw;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 public class DrawingProgram {
@@ -10,10 +7,6 @@ public class DrawingProgram {
     private final Canvas canvas = new Canvas();
 
     private final PrintStream printStream;
-
-    public DrawingProgram() {
-        this(System.out, () -> System.exit(0));
-    }
 
     public DrawingProgram(PrintStream printStream, ProgramExiter programExiter) {
         this.printStream = printStream;
@@ -29,22 +22,5 @@ public class DrawingProgram {
         }
 
         printStream.flush();
-    }
-
-    private void start() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String input;
-
-        printStream.print("enter command: ");
-
-        while((input=br.readLine())!=null){
-            enterCommand(input);
-            printStream.print("enter command: ");
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        new DrawingProgram().start();
     }
 }
---
+++ src/com/springer/draw/DrawingProgramCLI.java
@@ -0,0 +1,24 @@
+package com.springer.draw;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+public class DrawingProgramCLI {
+    public static void main(String[] args) throws IOException {
+        PrintStream printStream = System.out;
+        DrawingProgram drawingProgram = new DrawingProgram(printStream, () -> System.exit(0));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input;
+
+        printStream.print("enter command: ");
+
+        while((input=br.readLine())!=null){
+            drawingProgram.enterCommand(input);
+            printStream.print("enter command: ");
+        }
+    }
+}
